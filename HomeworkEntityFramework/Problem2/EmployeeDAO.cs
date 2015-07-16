0cdc6112d877696bce3c704741b0277fbeb814c6
Gleomit <draychev95@mail.ru>
2015-07-17 01:09:55 +0300

    problem 2 semi finished

---
+++ HomeworkEntityFramework/Problem2/EmployeeDAO.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using Problem1;
+
+namespace Problem2
+{
+    public static class EmployeeDAO
+    {
+        public static void Add(Employee employee)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                context.Employees.Add(employee);
+                context.SaveChanges();
+            }     
+        }
+
+        public static Employee FindByKey(object key)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                return context.Employees.Find((int)key);
+            }
+        }
+
+        public static void Modify(Employee employee)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                Employee empl = context.Employees.Find(employee.EmployeeID);
+
+                if (empl != null)
+                {
+                    empl = employee;
+                }
+
+                context.Entry(empl).State = EntityState.Modified;
+
+                context.SaveChanges();
+            }
+        }
+
+        public static void Delete(Employee employee)
+        {
+            using (var context = new SoftUniEntities())
+            {
+                context.Employees.Remove(employee);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ HomeworkEntityFramework/Problem2/TestEmployeeDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Problem1;
+
+namespace Problem2
+{
+    public class TestEmployeeDAO
+    {
+        public static void Main()
+        {
+            var employee = EmployeeDAO.FindByKey(297);
+
+            Console.WriteLine(employee.ToString());
+
+            EmployeeDAO.Delete(employee);
+
+            Console.Read();
+        }
+    }
+}
